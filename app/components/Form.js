7c03e97bfe0f859bf770084c5226113ed98a8f99
Ricardo Martins dos Reis <ricardomreis22@hotmail.com>
2023-11-22 16:50:42 +0000

    insert message errors in contact form

--- app/components/Form.js
+++ app/components/Form.js
@@ -65,24 +65,6 @@ export const Form = () => {
     return errors;
   };
 
-  // const Form = () => {
-  //   const [data, setData] = useState({
-  //     name: "",
-  //     email: "",
-  //     phone: "",
-  //     message: "",
-  //   });
-
-  //   const handleChange = (e) => {
-  //     const name = e.target.value;
-  //     const value = e.target.value;
-  //     setData({ ...data, [name]: value });
-  //   };
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     alert(data);
-  //   };
-
   return (
     <div className="flex flex-col mt-4 mx-4 md:w-1/2 xl:w-1/3 text-white">
       {Object.keys(formErrors).length === 0 && isSubmit && (
